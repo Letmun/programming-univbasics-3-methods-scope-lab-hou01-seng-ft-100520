f58e407f8bbe7def1ceba4e2acae56d4be999a88
Duncan Stark <letmunplaysgames@gmail.com>
2020-09-21 22:44:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -10,3 +10,9 @@ end
 def link
   puts take = "It's Dangerous To Go Alone! Take This."
 end
+
+def all_phrases
+  puts "It's-a me, Mario!"
+  puts 'Thank You Mario! But Our Princess Is In Another Castle!'
+  puts "It's Dangerous To Go Alone! Take This."
+end
